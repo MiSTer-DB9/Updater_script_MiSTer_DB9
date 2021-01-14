2aa55977178a503ec2d0f64cdc262de55c8916d3
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-01-14 23:07:45 +0100

    Considering only forks in SYNCING_FORKS

--- .github/create_cores_db.sh
+++ .github/create_cores_db.sh
@@ -6,6 +6,25 @@ set -euo pipefail
 CURL_RETRY="--connect-timeout 15 --max-time 120 --retry 3 --retry-delay 5"
 SSL_SECURITY_OPTION=""
 MISTER_DEVEL_REPOS_URL="https://api.github.com/users/mister-db9/repos"
+FORKS_INI_URL="https://raw.githubusercontent.com/MiSTer-DB9/Forks_MiSTer/master/Forks.ini"
+
+source <(curl ${CURL_RETRY} ${SSL_SECURITY_OPTION} "${FORKS_INI_URL}" | python -c "
+import sys, ConfigParser
+config = ConfigParser.ConfigParser()
+config.readfp(sys.stdin)
+for sec in config.sections():
+    print \"declare -A %s\" % (sec)
+    for key, val in config.items(sec):
+        print '%s[%s]=\"%s\"' % (sec, key, val)
+")
+
+declare -a CORES
+for i in ${Forks[syncing_forks]}
+do
+    declare -n fork="${i}"
+    CORES+=("${fork[release_core_name]}")
+done
+CORES+=("SD-Installer-Win64_MiSTer")
 
 ALL_REPOSITORIES=()
 API_PAGE=0
@@ -22,7 +41,10 @@ DB_FILE=${DB_FILE:-"./.github/cores_db.txt"}
 echo -n "" > "${DB_FILE}"
 for line in "${ALL_REPOSITORIES[@]} "
 do
-   echo "${line}" >> "${DB_FILE}"
+    CORE_NAME="${line%%|*}"
+    if [[ " ${CORES[@]} " =~ " ${CORE_NAME} " ]]; then
+        echo "${line}" >> "${DB_FILE}"
+    fi
 done
 
 if [[ "${1:-}" == "--early-exit" ]] ; then
